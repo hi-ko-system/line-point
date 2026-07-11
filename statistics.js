43c7ad5c2261c93d69d2fcff07a8f3a5404bfb16
hi-ko-system <hijiwa.korean@gmail.com>
2026-07-11 21:51:11 +0900

    Create statistics.js

---
+++ statistics.js
@@ -0,0 +1,132 @@
+const API_URL =
+"https://script.google.com/macros/s/AKfycbwJv-uJZakDqh4ryX83qet2ye9-00QsAalBnIMJkCphhOjqEKWC2wggiwoL8rrys1bi/exec";
+
+
+
+
+
+async function search(){
+
+
+const startDate =
+document.getElementById("startDate").value;
+
+
+const endDate =
+document.getElementById("endDate").value;
+
+
+
+if(!startDate || !endDate){
+
+alert("날짜를 선택해주세요");
+
+return;
+
+}
+
+
+
+const response =
+await fetch(
+API_URL
++
+"?action=searchHistory"
++
+"&startDate="
++
+startDate
++
+"&endDate="
++
+endDate
+);
+
+
+
+const data =
+await response.json();
+
+
+
+let charge=0;
+
+let use=0;
+
+
+
+const box =
+document.getElementById("history");
+
+
+box.innerHTML="";
+
+
+
+data.history.forEach(item=>{
+
+
+if(item.type==="충전"){
+
+charge += Number(item.amount);
+
+}else{
+
+use += Number(item.amount);
+
+}
+
+
+
+box.innerHTML +=
+
+`
+
+<div>
+
+<b>
+${item.name}
+</b>
+
+<br>
+
+${item.type}
+
+${item.type==="충전"?"+":"-"}
+
+${Number(item.amount).toLocaleString()}P
+
+<br>
+
+${item.memo}
+
+<br>
+
+${item.date}
+
+</div>
+
+<hr>
+
+`;
+
+
+
+});
+
+
+
+
+document.getElementById("totalCharge")
+.innerText =
+charge.toLocaleString()+"P";
+
+
+
+document.getElementById("totalUse")
+.innerText =
+use.toLocaleString()+"P";
+
+
+
+}
